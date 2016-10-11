49b9bf0f531e5630ef5ab936cab98fbac89531bb
ppsmillenium <ppscentury@hotmail.com>
2016-10-11 20:22:31 +0200

    Produccion_111016

---
+++ js/gourmetmallorca.js
@@ -0,0 +1,50 @@
+/*Funcion para mostrar div al pulsar enlace*/
+
+function mostrar(){
+	document.getElementById('platos').style.display = 'block';
+	
+}
+
+/**
+ * Funcion para cambiar la imagen de fondo
+ */
+
+function changeImg()
+{
+
+
+	document.getElementById('imgMeson').src = "img/variado2.jpg";
+	
+	document.getElementById('Entranteplato1').style.display = 'block';
+
+    /* document.getElementById("variado").style.textDecoration="underline"; */
+}
+
+
+function changeImg2()
+{
+
+
+	document.getElementById('imgMeson').src = "/img/caracoles_allioli.jpg";
+	
+	document.getElementById('Entranteplato2').style.display = 'block';
+
+    /* document.getElementById("variado").style.textDecoration="underline"; */
+}
+
+
+
+function changeImg3()
+{
+
+
+	document.getElementById('imgMeson').src = "/img/frito_mallorquin.jpg";
+	
+	document.getElementById('Entranteplato3').style.display = 'block';
+
+    /* document.getElementById("variado").style.textDecoration="underline"; */
+}
+
+
+
+
